e0d0215a685cbc49c09ae7c2b403a96f9074996b
khj <zla1275@gmail.com>
2020-04-22 14:16:43 +0900

    api로 데이터 받아와서 뿌리기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-    return (
-        <div className="App">
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
+
+    getMovies = async () => {
+        const {
+            data: {
+                data: { movies },
+            },
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+        this.setState({ movies, isLoading: false });
+    };
+
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    render() {
+        const { isLoading, movies } = this.state;
+        return (
             <div>
-                <h1>Hello</h1>
-                <h2>world</h2>
-                <h1>hi</h1>
+                {isLoading
+                    ? "loading"
+                    : movies.map((movie) => {
+                          return (
+                              <Movie
+                                  key={movie.id}
+                                  id={movie.id}
+                                  year={movie.year}
+                                  title={movie.title}
+                                  summary={movie.summary}
+                                  poster={movie.medium_cover_image}
+                              />
+                          );
+                      })}
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
